d2b6f8e5f506f84336643f6d4268774428aeb107
Adeoye Adefemi Opeoluwa <adefemiadeoye@yahoo.com>
2022-12-28 23:57:34 +0100

    CHORE

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import Controllers from "../controllers";
 const router = express.Router();
 
+// the routes gies hete
 router.post("/seed", Controllers.saveResponse);
 router.get("/seed", Controllers.getResponses)
 router.post("/bot", Controllers.chat)
